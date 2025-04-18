875a569fa45269775081b9a7c3a026f7c40e7bf5
dahyoji <gkdud8429@naver.com>
2025-04-18 14:28:39 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,66 @@
+$(function () {
+    $('.main_visual_slide').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        autoplaySpeed: 6000,
+        speed: 1000,
+    });
+
+
+    $('.main_visual .arrows .prev').on('click', function () {
+        $('.main_visual_slide').slick('slickPrev');
+    });
+
+    $('.main_visual .arrows .next').on('click', function () {
+        $('.main_visual_slide').slick('slickNext');
+    });
+
+
+    $('.main_content_slide').slick({
+        slidesToShow: 4,
+        arrows: false,
+        dots: true,
+    });
+
+    $('.main_content .arrows .prev').on('click', function () {
+        $('.main_content_slide').slick('slickPrev');
+    });
+
+    $('.main_content .arrows .next').on('click', function () {
+        $('.main_content_slide').slick('slickNext');
+    });
+
+});
+
+
+$(function () {
+
+    $('.main_product .tab_menu button').on('click', function () {
+        let idx = $(this).index(); // 클릭한 것의 번호를 가져와서 저장함
+        console.log(idx);
+
+        $('.main_product .tab_menu button').removeClass('on');
+        $(this).addClass('on');
+
+        $('.main_product .tab_content .con').removeClass('on');
+        $('.main_product .tab_content .con').eq(idx).addClass('on');
+    });
+
+});
+
+$(function () {
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 1000)
+    })
+});
+
+$(function () {
+    $('.footer #lnk').on('change', function () {
+        let lnk = $(this).val();
+        if (lnk) {
+            window.open(lnk)
+
+        }
+    })
+})
