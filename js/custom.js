a8ff9a81d53fa4a96eab7cc20c08d147a248e734
dahyoji <gkdud8429@naver.com>
2025-07-07 16:28:42 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,23 @@
 $(function () {
+    //스크롤이 됐을 때 header에 on을 붙인다.
+    //스크롤이 0이면 header에서 on을 뺀다.
+    //event : click, scroll, wheel, mouseenter, mouseleave, change
+
+    $(window).on('scroll', function () {
+        //스크롤 된 양을 구함.
+        let sct = $(window).scrollTop();
+        console.log(sct)
+
+        if (sct > 0) {
+            $('.header').addClass('on')
+        } else {
+            $('.header').removeClass('on')
+        }
+    })
+});
+
+
+$(function () {
     $('.main_visual_slide').slick({
         arrows: false,
         autoplay: true,
